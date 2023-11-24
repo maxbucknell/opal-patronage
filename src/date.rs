460ca0cf15676cc06105f252f41911f1038bfbe6
Max Bucknell <me@maxbucknell.com>
2023-11-24 12:22:46 +1100

    Add unit tests for date parsing

--- src/date.rs
+++ src/date.rs
@@ -4,16 +4,28 @@ use regex::Regex;
 
 use crate::error::{Error, ErrorKind};
 
-pub fn parse_and_clamp(min: DateTime<Tz>, max: DateTime<Tz>, date: &str) -> Result<DateTime<Tz>, Error> {
-    parse_date(date).and_then(|d| Ok(d.clamp(min, max)))
-}
-
 pub fn get_bounding_times(now: &DateTime<Utc>) -> (DateTime<Tz>, DateTime<Tz>) {
     let today = now.with_timezone(&Australia::NSW).duration_trunc(Duration::days(1)).unwrap();
     let min_start = Australia::NSW.with_ymd_and_hms(2020, 01, 01, 0, 0, 0).unwrap();
     let max_end = today - Duration::days(5);
 
     (min_start, max_end)
+}
+
+pub fn parse_and_clamp(min: DateTime<Tz>, max: DateTime<Tz>, date: &str) -> Result<DateTime<Tz>, Error> {
+    parse_date(date).and_then(|d| Ok(d.clamp(min, max)))
+}
+
+fn parse_date(s: &str) -> Result<DateTime<Tz>, Error> {
+    let [y, m, d] = date_string_to_components(s)
+        .and_then(|c| components_to_numbers(c))?;
+
+    let date = Australia::NSW.with_ymd_and_hms(y as i32, m as u32, d as u32, 0, 0, 0);
+
+    match date {
+        LocalResult::Single(d) => Ok(d),
+        _ => Err(Error::new(ErrorKind::NoSingleDate(y, m, d)))
+    }
 }
 
 fn date_string_to_components(s: &str) -> Result<[&str; 3], Error> {
@@ -74,14 +86,56 @@ fn days_in_month(y: u16, m: u16) -> u16 {
     }
 }
 
-fn parse_date(s: &str) -> Result<DateTime<Tz>, Error> {
-    let [y, m, d] = date_string_to_components(s)
-        .and_then(|c| components_to_numbers(c))?;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let date = Australia::NSW.with_ymd_and_hms(y as i32, m as u32, d as u32, 0, 0, 0);
+    #[test]
+    fn test_parse_date() {
+        assert_eq!(parse_date("2000-01-01").unwrap().to_rfc3339(), "2000-01-01T00:00:00+11:00");
+        assert_eq!(parse_date("2023-11-04").unwrap().to_rfc3339(), "2023-11-04T00:00:00+11:00");
+        assert_eq!(parse_date("2022-07-23").unwrap().to_rfc3339(), "2022-07-23T00:00:00+10:00");
 
-    match date {
-        LocalResult::Single(d) => Ok(d),
-        _ => Err(Error::new(ErrorKind::NoSingleDate(y, m, d)))
+        assert!(parse_date("2022-17-23").is_err());
+    }
+
+    #[test]
+    fn test_date_string_to_components() {
+        assert_eq!(date_string_to_components("1988-04-20").unwrap(), ["1988", "04", "20"]);
+        assert_eq!(date_string_to_components("2000-02-01").unwrap(), ["2000", "02", "01"]);
+        assert_eq!(date_string_to_components("1969-12-31").unwrap(), ["1969", "12", "31"]);
+
+        assert!(date_string_to_components("2002-123-19").is_err());
+        assert!(date_string_to_components("hello, world").is_err());
+    }
+
+    #[test]
+    fn test_components_to_numbers() {
+        assert_eq!(components_to_numbers(["2023", "11", "30"]).unwrap(), [2023, 11, 30]);
+        assert_eq!(components_to_numbers(["1998", "12", "31"]).unwrap(), [1998, 12, 31]);
+        assert_eq!(components_to_numbers(["0000", "01", "01"]).unwrap(), [0, 1, 1]);
+
+        assert!(components_to_numbers(["0000", "00", "01"]).is_err());
+        assert!(components_to_numbers(["1998", "12", "32"]).is_err());
+    }
+
+    #[test]
+    fn test_days_in_month() {
+        assert_eq!(days_in_month(2023, 1), 31);
+        assert_eq!(days_in_month(2023, 2), 28);
+        assert_eq!(days_in_month(2023, 3), 31);
+        assert_eq!(days_in_month(2023, 4), 30);
+        assert_eq!(days_in_month(2023, 5), 31);
+        assert_eq!(days_in_month(2023, 6), 30);
+        assert_eq!(days_in_month(2023, 7), 31);
+        assert_eq!(days_in_month(2023, 8), 31);
+        assert_eq!(days_in_month(2023, 9), 30);
+        assert_eq!(days_in_month(2023, 10), 31);
+        assert_eq!(days_in_month(2023, 11), 30);
+        assert_eq!(days_in_month(2023, 12), 31);
+
+        assert_eq!(days_in_month(2020, 2), 29);
+        assert_eq!(days_in_month(2000, 2), 29);
+        assert_eq!(days_in_month(2100, 2), 28);
     }
 }
